e7b4a9956e005ba30a18cb9eef42c63bae9946b2
Ben <benknight135@gmail.com>
2020-07-19 00:23:29 +0100

    add with python options and re-arrange options

--- build.bat
+++ build.bat
@@ -8,68 +8,84 @@ cd %scriptpath:~0,-1%
 echo ================
 echo Options
 echo ================
-:: get version from options file (first line)
+:: get version from options file (1st line)
 set /p opencv_version_text=< options.txt
 for /f "tokens=2 delims==" %%a in ("%opencv_version_text%") do (
   set opencv_version=%%a
 )
 echo opencv_version:%opencv_version%
 
-:: get with_contrib option from options file (second line)
-set "with_contrib_text="
-for /F "skip=1 delims=" %%i in (options.txt) do if not defined with_contrib_text set "with_contrib_text=%%i"
-for /f "tokens=2 delims==" %%a in ("%with_contrib_text%") do (
-  set with_contrib=%%a
+:: get visual_studio_version option from options file (2nd line)
+set "visual_studio_version_text="
+for /F "skip=3 delims=" %%i in (options.txt) do if not defined visual_studio_version_text set "visual_studio_version_text=%%i"
+for /f "tokens=2 delims==" %%a in ("%visual_studio_version_text%") do (
+  set visual_studio_version=%%a
 )
-echo with_contrib:%with_contrib%
+echo visual_studio_version:%visual_studio_version%
 
-:: get with_cuda option from options file (third line)
-set "with_cuda_text="
-for /F "skip=2 delims=" %%i in (options.txt) do if not defined with_cuda_text set "with_cuda_text=%%i"
-for /f "tokens=2 delims==" %%a in ("%with_cuda_text%") do (
-  set with_cuda=%%a
-)
-echo with_cuda:%with_cuda%
-
-:: get with_examples option from options file (4th line)
-set "with_examples_text="
-for /F "skip=3 delims=" %%i in (options.txt) do if not defined with_examples_text set "with_examples_text=%%i"
-for /f "tokens=2 delims==" %%a in ("%with_examples_text%") do (
-  set with_examples=%%a
-)
-echo with_examples:%with_examples%
-
-:: get build_world option from options file (5th line)
-set "build_world_text="
-for /F "skip=4 delims=" %%i in (options.txt) do if not defined build_world_text set "build_world_text=%%i"
-for /f "tokens=2 delims==" %%a in ("%build_world_text%") do (
-  set build_world=%%a
-)
-echo build_world:%build_world%
-
-:: get with_debug option from options file (6th line)
-set "with_debug_text="
-for /F "skip=5 delims=" %%i in (options.txt) do if not defined with_debug_text set "with_debug_text=%%i"
-for /f "tokens=2 delims==" %%a in ("%with_debug_text%") do (
-  set with_debug=%%a
-)
-echo with_debug:%with_debug%
-
-:: get clean_on_build option from options file (7th line)
+:: get clean_on_build option from options file (3rd line)
 set "clean_on_build_text="
-for /F "skip=6 delims=" %%i in (options.txt) do if not defined clean_on_build_text set "clean_on_build_text=%%i"
+for /F "skip=4 delims=" %%i in (options.txt) do if not defined clean_on_build_text set "clean_on_build_text=%%i"
 for /f "tokens=2 delims==" %%a in ("%clean_on_build_text%") do (
   set clean_on_build=%%a
 )
 echo clean_on_build:%clean_on_build%
 
-:: get visual_studio_version option from options file (8th line)
-set "visual_studio_version_text="
-for /F "skip=7 delims=" %%i in (options.txt) do if not defined visual_studio_version_text set "visual_studio_version_text=%%i"
-for /f "tokens=2 delims==" %%a in ("%visual_studio_version_text%") do (
-  set visual_studio_version=%%a
+:: get with_contrib option from options file (4th line)
+set "with_contrib_text="
+for /F "skip=5 delims=" %%i in (options.txt) do if not defined with_contrib_text set "with_contrib_text=%%i"
+for /f "tokens=2 delims==" %%a in ("%with_contrib_text%") do (
+  set with_contrib=%%a
 )
-echo visual_studio_version:%visual_studio_version%
+echo with_contrib:%with_contrib%
+
+:: get with_cuda option from options file (5th line)
+set "with_cuda_text="
+for /F "skip=6 delims=" %%i in (options.txt) do if not defined with_cuda_text set "with_cuda_text=%%i"
+for /f "tokens=2 delims==" %%a in ("%with_cuda_text%") do (
+  set with_cuda=%%a
+)
+echo with_cuda:%with_cuda%
+
+:: get with_examples option from options file (6th line)
+set "with_examples_text="
+for /F "skip=7 delims=" %%i in (options.txt) do if not defined with_examples_text set "with_examples_text=%%i"
+for /f "tokens=2 delims==" %%a in ("%with_examples_text%") do (
+  set with_examples=%%a
+)
+echo with_examples:%with_examples%
+
+:: get build_world option from options file (7th line)
+set "build_world_text="
+for /F "skip=8 delims=" %%i in (options.txt) do if not defined build_world_text set "build_world_text=%%i"
+for /f "tokens=2 delims==" %%a in ("%build_world_text%") do (
+  set build_world=%%a
+)
+echo build_world:%build_world%
+
+:: get with_debug option from options file (8th line)
+set "with_debug_text="
+for /F "skip=9 delims=" %%i in (options.txt) do if not defined with_debug_text set "with_debug_text=%%i"
+for /f "tokens=2 delims==" %%a in ("%with_debug_text%") do (
+  set with_debug=%%a
+)
+echo with_debug:%with_debug%
+
+:: get with_python_release option from options file (9th line)
+set "with_python_release_text="
+for /F "skip=10 delims=" %%i in (options.txt) do if not defined with_python_release_text set "with_python_release_text=%%i"
+for /f "tokens=2 delims==" %%a in ("%with_python_release_text%") do (
+  set with_python_release=%%a
+)
+echo with_python_release:%with_python_release%
+
+:: get with_python_release option from options file (10th line)
+set "with_python_debug_text="
+for /F "skip=6 delims=" %%i in (options.txt) do if not defined with_python_debug_text set "with_python_debug_text=%%i"
+for /f "tokens=2 delims==" %%a in ("%with_python_debug_text%") do (
+  set with_python_debug=%%a
+)
+echo with_python_debug:%with_python_debug%
 
 echo ================
 
@@ -113,6 +129,20 @@ if "%build_world%" == "true" (
     set cmake_build_world=OFF
 )
 
+:: define python_release as on/off
+if "%with_python_release%" == "true" (
+    set cmake_python_release=ON
+) else (
+    set cmake_python_release=OFF
+)
+
+:: define python_debug as on/off
+if "%with_python_debug%" == "true" (
+    set cmake_python_debug=ON
+) else (
+    set cmake_python_debug=OFF
+)
+
 :: define contrib only options
 if "%with_contrib%" == "true" (
     set cmake_extra_modules_path=%cd%/opencv_contrib/modules
@@ -134,7 +164,11 @@ if "%with_cuda%" == "true" (
 )
 
 :: build opencv release
-cd %build_release_folder%
+if "%with_debug%" == "true" (
+  cd %build_release_folder%
+) else (
+  cd %build_folder%
+)
 cmake -G "%visual_studio_version%" ^
     -D CMAKE_BUILD_TYPE=RELEASE ^
     -D CMAKE_INSTALL_PREFIX=%install_release_folder% ^
@@ -145,6 +179,9 @@ cmake -G "%visual_studio_version%" ^
     -D WITH_CUBLAS=%cmake_with_cublas% ^
     -D INSTALL_PYTHON_EXAMPLES=%cmake_with_cuda% ^
     -D OPENCV_EXTRA_MODULES_PATH=%cmake_extra_modules_path% ^
+    -D BUILD_opencv_python=%cmake_python_release% ^
+    -D BUILD_opencv_python3=%cmake_python_release% ^
+    -D BUILD_opencv_python2=%cmake_python_release% ^
     -D BUILD_EXAMPLES=%cmake_with_cuda% ..\..
 
 if "%with_debug%" == "true" (
@@ -160,17 +197,23 @@ if "%with_debug%" == "true" (
       -D WITH_CUBLAS=%cmake_with_cublas% ^
       -D INSTALL_PYTHON_EXAMPLES=%cmake_with_cuda% ^
       -D OPENCV_EXTRA_MODULES_PATH=%cmake_extra_modules_path% ^
-      -D BUILD_opencv_python=OFF ^
-      -D BUILD_opencv_python3=OFF ^
-      -D BUILD_opencv_python2=OFF ^
+      -D BUILD_opencv_python=%cmake_python_debug% ^
+      -D BUILD_opencv_python3=%cmake_python_debug% ^
+      -D BUILD_opencv_python2=%cmake_python_debug% ^
       -D BUILD_EXAMPLES=%cmake_with_cuda% ..\..
 )
 
 :: install opencv release
+if "%with_debug%" == "true" (
+  cd %build_release_folder%
+) else (
+  cd %build_folder%
+)
 cmake --build . --config Release --target install
 
 if "%with_debug%" == "true" (
   :: install opencv debug
+  cd %build_debug_folder%
   cmake --build . --config Debug --target install
 )
 
--- download.bat
+++ download.bat
@@ -8,68 +8,84 @@ cd %scriptpath:~0,-1%
 echo ================
 echo Options
 echo ================
-:: get version from options file (first line)
+:: get version from options file (1st line)
 set /p opencv_version_text=< options.txt
 for /f "tokens=2 delims==" %%a in ("%opencv_version_text%") do (
   set opencv_version=%%a
 )
 echo opencv_version:%opencv_version%
 
-:: get with_contrib option from options file (second line)
-set "with_contrib_text="
-for /F "skip=1 delims=" %%i in (options.txt) do if not defined with_contrib_text set "with_contrib_text=%%i"
-for /f "tokens=2 delims==" %%a in ("%with_contrib_text%") do (
-  set with_contrib=%%a
+:: get visual_studio_version option from options file (2nd line)
+set "visual_studio_version_text="
+for /F "skip=3 delims=" %%i in (options.txt) do if not defined visual_studio_version_text set "visual_studio_version_text=%%i"
+for /f "tokens=2 delims==" %%a in ("%visual_studio_version_text%") do (
+  set visual_studio_version=%%a
 )
-echo with_contrib:%with_contrib%
+echo visual_studio_version:%visual_studio_version%
 
-:: get with_cuda option from options file (third line)
-set "with_cuda_text="
-for /F "skip=2 delims=" %%i in (options.txt) do if not defined with_cuda_text set "with_cuda_text=%%i"
-for /f "tokens=2 delims==" %%a in ("%with_cuda_text%") do (
-  set with_cuda=%%a
-)
-echo with_cuda:%with_cuda%
-
-:: get with_examples option from options file (4th line)
-set "with_examples_text="
-for /F "skip=3 delims=" %%i in (options.txt) do if not defined with_examples_text set "with_examples_text=%%i"
-for /f "tokens=2 delims==" %%a in ("%with_examples_text%") do (
-  set with_examples=%%a
-)
-echo with_examples:%with_examples%
-
-:: get build_world option from options file (5th line)
-set "build_world_text="
-for /F "skip=4 delims=" %%i in (options.txt) do if not defined build_world_text set "build_world_text=%%i"
-for /f "tokens=2 delims==" %%a in ("%build_world_text%") do (
-  set build_world=%%a
-)
-echo build_world:%build_world%
-
-:: get with_debug option from options file (6th line)
-set "with_debug_text="
-for /F "skip=5 delims=" %%i in (options.txt) do if not defined with_debug_text set "with_debug_text=%%i"
-for /f "tokens=2 delims==" %%a in ("%with_debug_text%") do (
-  set with_debug=%%a
-)
-echo with_debug:%with_debug%
-
-:: get clean_on_build option from options file (7th line)
+:: get clean_on_build option from options file (3rd line)
 set "clean_on_build_text="
-for /F "skip=6 delims=" %%i in (options.txt) do if not defined clean_on_build_text set "clean_on_build_text=%%i"
+for /F "skip=4 delims=" %%i in (options.txt) do if not defined clean_on_build_text set "clean_on_build_text=%%i"
 for /f "tokens=2 delims==" %%a in ("%clean_on_build_text%") do (
   set clean_on_build=%%a
 )
 echo clean_on_build:%clean_on_build%
 
-:: get visual_studio_version option from options file (8th line)
-set "visual_studio_version_text="
-for /F "skip=7 delims=" %%i in (options.txt) do if not defined visual_studio_version_text set "visual_studio_version_text=%%i"
-for /f "tokens=2 delims==" %%a in ("%visual_studio_version_text%") do (
-  set visual_studio_version=%%a
+:: get with_contrib option from options file (4th line)
+set "with_contrib_text="
+for /F "skip=5 delims=" %%i in (options.txt) do if not defined with_contrib_text set "with_contrib_text=%%i"
+for /f "tokens=2 delims==" %%a in ("%with_contrib_text%") do (
+  set with_contrib=%%a
 )
-echo visual_studio_version:%visual_studio_version%
+echo with_contrib:%with_contrib%
+
+:: get with_cuda option from options file (5th line)
+set "with_cuda_text="
+for /F "skip=6 delims=" %%i in (options.txt) do if not defined with_cuda_text set "with_cuda_text=%%i"
+for /f "tokens=2 delims==" %%a in ("%with_cuda_text%") do (
+  set with_cuda=%%a
+)
+echo with_cuda:%with_cuda%
+
+:: get with_examples option from options file (6th line)
+set "with_examples_text="
+for /F "skip=7 delims=" %%i in (options.txt) do if not defined with_examples_text set "with_examples_text=%%i"
+for /f "tokens=2 delims==" %%a in ("%with_examples_text%") do (
+  set with_examples=%%a
+)
+echo with_examples:%with_examples%
+
+:: get build_world option from options file (7th line)
+set "build_world_text="
+for /F "skip=8 delims=" %%i in (options.txt) do if not defined build_world_text set "build_world_text=%%i"
+for /f "tokens=2 delims==" %%a in ("%build_world_text%") do (
+  set build_world=%%a
+)
+echo build_world:%build_world%
+
+:: get with_debug option from options file (8th line)
+set "with_debug_text="
+for /F "skip=9 delims=" %%i in (options.txt) do if not defined with_debug_text set "with_debug_text=%%i"
+for /f "tokens=2 delims==" %%a in ("%with_debug_text%") do (
+  set with_debug=%%a
+)
+echo with_debug:%with_debug%
+
+:: get with_python_release option from options file (9th line)
+set "with_python_release_text="
+for /F "skip=10 delims=" %%i in (options.txt) do if not defined with_python_release_text set "with_python_release_text=%%i"
+for /f "tokens=2 delims==" %%a in ("%with_python_release_text%") do (
+  set with_python_release=%%a
+)
+echo with_python_release:%with_python_release%
+
+:: get with_python_release option from options file (10th line)
+set "with_python_debug_text="
+for /F "skip=6 delims=" %%i in (options.txt) do if not defined with_python_debug_text set "with_python_debug_text=%%i"
+for /f "tokens=2 delims==" %%a in ("%with_python_debug_text%") do (
+  set with_python_debug=%%a
+)
+echo with_python_debug:%with_python_debug%
 
 echo ================
 
